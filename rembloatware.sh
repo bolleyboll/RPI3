9a4a7c6ab2bf3e1c9608810885f8c064ee11d06d
bolleyboll <31176934+bolleyboll@users.noreply.github.com>
2018-04-24 16:43:58 +0530

    Fixed \r bugs.

--- rembloatware.sh
+++ rembloatware.sh
@@ -1,14 +1,17 @@
-echo "Removing useless Packages..."
-su
+echo "Removing Useless Packages..."
 
-sudo apt-get purge wolfram-engine libreoffice* scratch* scratch2 greenfoot nodered claws-mail claws-mail-i18n minecraft-pi python-pygame -y
+sudo apt-get purge wolfram-engine libreoffice* geany scratch* scratch2 greenfoot nodered claws-mail claws-mail-i18n minecraft-pi python-pygame -y
 
+echo "Removing PyGames"
 sudo rm -R /home/pi/python_games
 rm -rf ~/python_games
 
 sudo apt-get clean
 sudo apt-get autoremove -y
+
 echo "Installing gedit"
 sudo apt-get install gedit
 df -h
+su
+read s
 reboot
